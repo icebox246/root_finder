d94ab0a62c9cb4c6a1bef53505723cfbf90d4fa2
icebox246 <iceboxpoczta@gmail.com>
2022-04-16 14:49:18 +0200

    Added correct tests!

--- test/roots_test.exs
+++ test/roots_test.exs
@@ -2,7 +2,11 @@ defmodule RootsTest do
   use ExUnit.Case
   doctest Roots
 
-  test "greets the world" do
-    assert Roots.hello() == :world
+  test "finds roots of x^2 + 4x + 4" do
+    assert Roots.roots([1, 4, 4]) == [-2, -2]
+  end
+
+  test "finds roots of 1000x^2 - 2x - 1" do
+    assert Roots.roots([1000, -2, -1]) == [-0.031, 0.033]
   end
 end
